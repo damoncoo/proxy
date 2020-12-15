d3822ece5808778d383d73a4eb478d8d212a9361
Darcy <damoncoo@gmail.com>
2020-12-15 18:03:06 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/damoncoo/proxy
+
+go 1.15
+
+require github.com/elazarl/goproxy v0.0.0-20201021153353-00ad82a08272
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/elazarl/goproxy"
+)
+
+func main() {
+	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
+	addr := flag.String("addr", ":8090", "proxy listen address")
+	flag.Parse()
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = *verbose
+
+	proxy.OnRequest().DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+
+		return req, nil
+	})
+
+	proxy.OnRequest().HandleConnect(goproxy.FuncHttpsHandler(func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
+
+		return goproxy.OkConnect, ""
+	}))
+
+	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+
+		return resp
+	})
+
+	go log.Fatal(http.ListenAndServe(*addr, proxy))
+}
